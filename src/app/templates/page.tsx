6b9504d2bf4a710c41c6ce40c859831b36373d33
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-01-10 12:08:35 +0500

    feat(template): :sparkles: list not found ui

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -3,7 +3,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import dayjs from "dayjs";
-import { File, FileText } from "lucide-react";
+import { File, FileText, Plus } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { $listTemplates } from "./actions";
@@ -17,40 +17,62 @@ export default async function Page() {
 
 	const items = result.data;
 
+	const createTemplate = (title = "New template") => (
+		<CreateTemplateDialog>
+			<Button>
+				<Plus />
+				{title}
+			</Button>
+		</CreateTemplateDialog>
+	);
+
 	return (
 		<div className="w-full p-6 h-full">
 			<div className="w-full flex justify-between items-center">
 				<h1 className="text-2xl font-bold">Templates</h1>
-				<CreateTemplateDialog>
-					<Button>Create template</Button>
-				</CreateTemplateDialog>
+				{createTemplate()}
 			</div>
-			<ul className="w-full grid grid-cols-5 gap-4">
-				{items.map((item) => (
-					<Link
-						href={`/templates/${item.id}`}
-						title={item.title}
-						key={item.id}
-						className="p-6 rounded-lg bg-secondary cursor-pointer relative"
-					>
-						<FileText size={32} />
-						<div className="grid mt-2">
-							<h2 className="line-clamp-1 font-bold">{item.title}</h2>
+			{
+				items.length < 1 ? (
+					<div className="w-full flex flex-col items-center mt-12">
+						<File size={32} />
+						<div className="flex flex-col items-center mt-2">
+							<h2 className="text-lg font-bold">No files found</h2>
 							<p className="text-sm text-muted-foreground">
-								{dayjs(item.createdAt).format("DD.MM.YYYY HH:mm")}
+								You should create new template
 							</p>
+							<div className="mt-2">{createTemplate('Create a new template')}</div>
 						</div>
-						<Badge
-							className={cn(
-								item.published && "bg-green-400",
-								"absolute top-6 right-6 ",
-							)}
-						>
-							{item.published ? "published" : "draft"}
-						</Badge>
-					</Link>
-				))}
-			</ul>
+					</div>
+				) : (
+					<ul className="w-full grid grid-cols-5 gap-4 mt-4">
+						{items.map((item) => (
+							<Link
+								href={`/templates/${item.id}`}
+								title={item.title}
+								key={item.id}
+								className="p-6 rounded-lg bg-secondary cursor-pointer relative"
+							>
+								<FileText size={32} />
+								<div className="grid mt-2">
+									<h2 className="line-clamp-1 font-bold">{item.title}</h2>
+									<p className="text-sm text-muted-foreground">
+										{dayjs(item.createdAt).format("DD.MM.YYYY HH:mm")}
+									</p>
+								</div>
+								<Badge
+									className={cn(
+										item.published && "bg-green-400",
+										"absolute top-6 right-6 ",
+									)}
+								>
+									{item.published ? "published" : "draft"}
+								</Badge>
+							</Link>
+						))}
+					</ul>
+				)
+			}
 		</div>
 	);
 }
